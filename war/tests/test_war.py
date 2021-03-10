615dc3dafb0d83cd6ee5c354af40ef8d0c424b4f
Dumitru-Claudiu <claudiu.sergentu@gmail.com>
2021-03-09 22:15:08 -0500

    unit tests for card class and deck class

--- war/tests/test_war.py
+++ war/tests/test_war.py
@@ -7,6 +7,8 @@ import pytest
 from unittest import mock
 import builtins
 from war.functions import get_players
+from war.card_class import Card
+from war.deck_class import Deck
 
 
 def test_get_players_mocking():
@@ -21,3 +23,17 @@ def test_get_players_monkeypatching(monkeypatch):
 
     monkeypatch.setattr('builtins.input', lambda _: "Flori")
     assert get_players() == ['Flori', 'Flori']
+
+
+def test_card_class():
+    """Testing the print dunder of a Card object"""
+
+    my_card = Card("Clubs", "Three", 3)
+    assert my_card.__str__() == "Three of Clubs, worth 3 points."
+
+
+def test_len_deck_class():
+    """Testing the len dunder of the Deck class"""
+
+    my_deck = Deck()
+    assert my_deck.__len__() == 52
